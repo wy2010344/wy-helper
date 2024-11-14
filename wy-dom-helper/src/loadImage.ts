b595a2003dc46490dda7ee9a92940300280a09de
wyknmjj <wyknmjj@protonmail.com>
2024-11-14 18:32:25 +0800

    add load image

---
+++ wy-dom-helper/src/loadImage.ts
@@ -0,0 +1,10 @@
+export function loadImage(url: string) {
+  const img = new Image()
+  return new Promise<HTMLImageElement>((resolve, reject) => {
+    img.onload = () => {
+      resolve(img)
+    }
+    img.onerror = reject
+    img.src = url
+  })
+}
