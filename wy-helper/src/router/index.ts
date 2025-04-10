a4a9996688263a6a715d95d76cd8a1c7ed721dae
wy2010344 <wangyang2010344@foxmail.com>
2025-04-10 19:28:22 +0800

    more route pretty

--- wy-helper/src/router/index.ts
+++ wy-helper/src/router/index.ts
@@ -1,4 +1,286 @@
-import { emptyObject, quote } from "../util"
+import { KVPair } from "../KVPair"
+import { emptyObject, Quote, quote } from "../util"
+import { matchMatch, MatchNode, toMatchNode } from "./match"
+
+
+
+type RouteBranch<BranchLoader, LeafLoader, MoreLoader> = {
+  //名称
+  key: string
+  match: MatchNode
+  order: number
+  //布局页面
+  layout?(): Promise<BranchLoader>
+  //无子路径页面
+  index?(): Promise<LeafLoader>
+  //如果所有匹配失败,则转向它
+  default?(): Promise<MoreLoader>
+  children?: RouteBranch<BranchLoader, LeafLoader, MoreLoader>[]
+}
+function sortBranch(a: RouteBranch<any, any, any>, b: RouteBranch<any, any, any>) {
+  return a.order - b.order
+}
+
+
+
+
+export class TreeRoute<BranchLoader, LeafLoader, MoreLoader> {
+  constructor(
+    private typeDefMap: Record<string, (v: string) => any>
+  ) { }
+  private rootBranch: RouteBranch<BranchLoader, LeafLoader, MoreLoader> = {
+    key: "",
+    match: "",
+    order: 0
+  }
+
+  buildFromMap(
+    pages: Record<string, () => Promise<any>>,
+    prefix: string,
+    INDEX = 'index',
+    LAYOUT = 'layout',
+    DEFAULT = 'default'
+  ) {
+    for (const key in pages) {
+      let queryPath = key.slice(prefix.length, key.lastIndexOf('.'))
+      if (queryPath.endsWith(INDEX)) {
+        const nodes = queryPath.slice(0, queryPath.length - INDEX.length).split('/').filter(quote)
+        const tempBranch = this.buildRoot(nodes)
+        tempBranch.index = pages[key] as any
+      }
+      if (queryPath.endsWith(LAYOUT)) {
+        const nodes = queryPath.slice(0, queryPath.length - LAYOUT.length).split('/').filter(quote)
+        const tempBranch = this.buildRoot(nodes)
+        tempBranch.layout = pages[key] as any
+      }
+
+      if (queryPath.endsWith(DEFAULT)) {
+        const nodes = queryPath.slice(0, queryPath.length - DEFAULT.length).split('/').filter(quote)
+        const tempBranch = this.buildRoot(nodes)
+        tempBranch.default = pages[key] as any
+      }
+    }
+  }
+  buildRoot(nodes: string[]) {
+    let tempBranch = this.rootBranch
+    for (let i = 0; i < nodes.length; i++) {
+      const node = nodes[i]
+      if (!tempBranch.children) {
+        tempBranch.children = []
+      }
+      let subBranch = tempBranch.children.find(x => x.key == node)
+      if (!subBranch) {
+        const sp = node.split('.')
+        let order = 0
+        let match = node
+        if (sp.length > 1) {
+          order = Number(sp[0])
+          if (isNaN(order)) {
+            order = 0
+          } else {
+            sp.shift()
+            match = sp.join('.')
+          }
+        }
+        subBranch = {
+          key: node,
+          match: toMatchNode(match, this.typeDefMap),
+          order
+        }
+        tempBranch.children.push(subBranch)
+      }
+      tempBranch = subBranch
+    }
+    return tempBranch
+  }
+
+  private sort(vs: RouteBranch<BranchLoader, LeafLoader, MoreLoader>[]) {
+    vs.sort(sortBranch)
+    for (let i = 0; i < vs.length; i++) {
+      const children = vs[i].children
+      if (children) {
+        this.sort(children)
+      }
+    }
+  }
+  finishBuild() {
+    const rootChildren = this.rootBranch.children
+    if (rootChildren) {
+      this.sort(rootChildren)
+    }
+    return rootChildren
+  }
+  matchNodes(nodes: string[]) {
+    const branches: BranchWithQuery<BranchLoader, LeafLoader, MoreLoader>[] = [
+      {
+        branch: this.rootBranch
+      }
+    ]
+    let tempBranch = this.rootBranch
+    let query: KVPair<any> | undefined = undefined
+
+    let findDefault = false
+    for (let i = 0; i < nodes.length && !findDefault; i++) {
+      const node = nodes[i]
+      try {
+        if (tempBranch.children) {
+          //寻找匹配的分支
+          let foundMath: RouteBranch<BranchLoader, LeafLoader, MoreLoader> | undefined = undefined
+          for (let i = 0; i < tempBranch.children.length && !foundMath; i++) {
+            const cacheTempBranch = tempBranch.children[i]
+            const match = cacheTempBranch.match
+            try {
+              query = matchMatch(node, match, query)
+              foundMath = cacheTempBranch
+            } catch (err) { }
+          }
+          if (foundMath) {
+            //@todo 这里,递进尝试,失败了,并不会返回上一处栈,最好使用递归
+            branches.push({
+              branch: foundMath,
+              query
+            })
+            tempBranch = foundMath
+          } else {
+            throw new Error('not found')
+          }
+        } else {
+          throw new Error('not found')
+        }
+      } catch (err) {
+        if (tempBranch.default) {
+          branches.push({
+            branch: tempBranch,
+            query,
+            restNodes: nodes.slice(i)
+          })
+          findDefault = true
+        } else {
+          throw err
+        }
+      }
+    }
+    return branches
+  }
+}
+
+
+type BranchWithQuery<BranchLoader, LeafLoader, MoreLoader> = {
+  branch: RouteBranch<BranchLoader, LeafLoader, MoreLoader>
+  query?: KVPair<any> | undefined
+  restNodes?: string[]
+}
+export type PairLeaf<LeafLoader> = {
+  default?: never
+  index(): Promise<LeafLoader>
+  layout?: never
+  next?: never
+  query: Readonly<Record<string, any>>
+  restNodes?: never
+}
+export type PairMore<MoreLoader> = {
+  index?: never
+  default(): Promise<MoreLoader>
+  layout?: never
+  next?: never
+  query: Readonly<Record<string, any>>
+  restNodes: string[]
+}
+export type PairBranch<BranchLoader, LeafLoader, MoreLoader> = {
+  default?: never
+  index?: never
+  layout(): Promise<BranchLoader>
+  next: PairBranch<BranchLoader, LeafLoader, MoreLoader> | PairLeaf<LeafLoader> | PairMore<MoreLoader>
+  query: Readonly<Record<string, any>>
+  restNodes?: never
+}
+
+export function branchesToPairs<BranchLoader, LeafLoader, MoreLoader>(branches: BranchWithQuery<BranchLoader, LeafLoader, MoreLoader>[], trans: <T>(v: T) => T = quote) {
+  const lastBranch = branches.at(-1)
+  if (!lastBranch) {
+    throw new Error('至少需要一个节点')
+  }
+  const query = lastBranch.query?.toObject() || emptyObject
+
+  let last: PairLeaf<LeafLoader> | PairMore<MoreLoader>
+  if (lastBranch.restNodes) {
+    last = {
+      default: trans(lastBranch.branch.default!),
+      query,
+      restNodes: lastBranch.restNodes
+    }
+  } else {
+    last = {
+      index: trans(lastBranch.branch.index!),
+      query
+    }
+  }
+  let ret: PairBranch<BranchLoader, LeafLoader, MoreLoader> | PairLeaf<LeafLoader> | PairMore<MoreLoader> = last
+  for (let i = branches.length - 1; i > -1; i--) {
+    const branch = branches[i]
+    if (branch.branch.layout) {
+      //新增一个layout
+      ret = {
+        next: ret,
+        layout: trans(branch.branch.layout),
+        query
+      }
+    }
+  }
+  return ret
+}
+/**
+ * 格式
+ *  /xxx/aa 匹配/xxx/aa
+ *  /xxx/[ab]-bc-[dd]/[bc]/... 这种规则其实有点类似于prolog
+ *     先要解析出规则,还要依规则去解析具体内容
+ *  /xxx/aa/... 匹配/xxx/aa及其之后
+ * @param queryPath 
+ * @param startWith 
+ * @returns 
+ */
+export function locationMatch(
+  queryPath: string,
+  typeDefMap: Record<string, (v: string) => any> = emptyObject
+): MatchRule {
+  let queryNodes = queryPath.split('/').filter(quote)
+  let ignoreMore = false
+  if (queryNodes.at(-1) == '...') {
+    queryNodes.pop()
+    ignoreMore = true
+  }
+  const rules = queryNodes.map(node => {
+    return toMatchNode(node, typeDefMap)
+  })
+  return function (_nodes: readonly string[]) {
+    const nodes = _nodes.slice()
+    const restNodes: string[] = []
+    if (ignoreMore) {
+      while (nodes.length > queryNodes.length) {
+        restNodes.unshift(nodes.pop()!)
+      }
+    }
+    if (nodes.length == rules.length) {
+      let queryObj: KVPair<any> | undefined = undefined
+      for (let i = 0; i < nodes.length; i++) {
+        const node = nodes[i]
+        const query = rules[i]
+        try {
+          queryObj = matchMatch(node, query, queryObj)
+        } catch {
+          return
+        }
+      }
+      return {
+        ignoreMore,
+        query: queryObj?.toObject() || emptyObject,
+        restNodes,
+        matchNodes: nodes
+      }
+    }
+  }
+}
+
 
 /**
  * 将路径按/拆分成节点
@@ -9,26 +291,6 @@ export function getPathNodes(pathname: string, split = '/') {
   pathname = decodeURI(pathname)
   return pathname.split(split).filter(quote)
 }
-class MatchNode {
-  constructor(
-    public readonly before: string,
-    public readonly key: string,
-    public readonly after: string,
-    public readonly type: (v: string) => any
-  ) { }
-  match(query: string) {
-    if (query.startsWith(this.before) && query.endsWith(this.after)) {
-      const value = query.slice(
-        query.indexOf(this.before) + this.before.length,
-        query.lastIndexOf(this.after))
-      if (!value) {
-        return
-      }
-      return this.type(value)
-    }
-  }
-}
-
 export type MatchRule = (pathNodes: readonly string[]) => void | {
   /**
    * 匹配的nodes
@@ -44,71 +306,6 @@ export type MatchRule = (pathNodes: readonly string[]) => void | {
    */
   query: Record<string, string>
 }
-/**
- * 格式
- *  /xxx/aa 匹配/xxx/aa
- *  /xxx/[ab]-bc-[dd]/[bc]/... 这种规则其实有点类似于prolog
- *     先要解析出规则,还要依规则去解析具体内容
- *  /xxx/aa/... 匹配/xxx/aa及其之后
- * @param queryPath 
- * @param startWith 
- * @returns 
- */
-export function locationMatch(queryPath: string, typeDefMap: Record<string, (v: string) => any> = emptyObject): MatchRule {
-  let queryNodes = queryPath.split('/').filter(quote)
-  let ignoreMore = false
-  if (queryNodes.at(-1) == '...') {
-    queryNodes.pop()
-    ignoreMore = true
-  }
-  const rules = queryNodes.map(node => {
-    const idx1 = node.indexOf('[')
-    if (idx1 < 0) {
-      return node
-    }
-    const idx2 = node.indexOf(']')
-    if (idx1 < idx2) {
-      const before = node.slice(0, idx1)
-      const varDef = node.slice(idx1 + 1, idx2).split(':')
-      const after = node.slice(idx2 + 1)
-      return new MatchNode(before, varDef[0], after, typeDefMap[varDef[1]] || quote)
-    }
-    throw new Error("变量区域需要匹配的]")
-  })
-  return function (_nodes: readonly string[]) {
-    const nodes = _nodes.slice()
-    const restNodes: string[] = []
-    if (ignoreMore) {
-      while (nodes.length > queryNodes.length) {
-        restNodes.unshift(nodes.pop()!)
-      }
-    }
-    if (nodes.length == rules.length) {
-      let queryObj: Record<string, any> = {}
-      for (let i = 0; i < nodes.length; i++) {
-        const node = nodes[i]
-        const query = rules[i]
-        if (query instanceof MatchNode) {
-          const value = query.match(node)
-          if (typeof value != 'undefined') {
-            queryObj[query.key] = value
-          } else {
-            return
-          }
-        } else if (node != query) {
-          return
-        }
-      }
-      return {
-        ignoreMore,
-        query: queryObj,
-        restNodes,
-        matchNodes: nodes
-      }
-    }
-  }
-}
-
 export abstract class RelativeHistory {
   push(v: string) {
     this.push_nodes(getPathNodes(v))
---
+++ wy-helper/src/router/match.ts
@@ -0,0 +1,157 @@
+
+import { KVPair } from "../KVPair"
+import { ruleGetString, manyMatch, matchCharNotIn, ruleGet, Que, manyRuleGet, andMatch, andRuleGet, matchCharIn, orRuleGet, alawaysGet, ParserSuccess } from "../tokenParser"
+import { quote } from "../util"
+
+export type MatchNode = string | {
+  before: string
+  key: string
+  type(v: string): any
+  next: MatchNode
+}
+const QuoteLeft = '['.charCodeAt(0)
+const QuoteRight = ']'.charCodeAt(0)
+const Colon = ':'.charCodeAt(0)
+const matchNotChat = matchCharNotIn(QuoteLeft, QuoteRight)
+const matchNotChatColon = matchCharNotIn(QuoteLeft, QuoteRight, Colon)
+const getStr0 = ruleGet<Que, string>(
+  manyMatch(
+    matchNotChat,
+  ),
+  ruleGetString
+)
+const ruleGetKey = ruleGet<Que, string>(manyMatch(
+  matchNotChatColon,
+  1
+), ruleGetString)
+/**
+ * [abc]
+ * [avd:dds]
+ */
+const getQuote = andRuleGet(
+  [
+    ruleGet(matchCharIn(QuoteLeft), quote),
+    ruleGetKey,
+    orRuleGet(
+      [
+        andRuleGet(
+          [
+            ruleGet(matchCharIn(Colon), quote),
+            ruleGetKey
+          ],
+          function (a, b) {
+            return b
+          }
+        ),
+        alawaysGet()
+      ],
+    ),
+    ruleGet(matchCharIn(QuoteRight), quote)
+  ],
+  function (a, b, c, d) {
+    return [b, c] as const
+  }
+)
+
+
+const parseRule = andRuleGet([
+  manyRuleGet(
+    andRuleGet(
+      [
+        getStr0,
+        getQuote
+      ],
+      function (a, b) {
+        return {
+          begin: a,
+          key: b[0],
+          type: b[1]
+        }
+      }
+    )
+  ),
+  getStr0
+], function (a, b) {
+  return {
+    before: a,
+    last: b
+  }
+})
+
+
+export function matchMatch(query: string, match: MatchNode, args?: KVPair<any>) {
+  let idx = 0
+  while (match) {
+    if (typeof match == 'string') {
+      if (query.endsWith(match)) {
+        return args
+      } else {
+        throw new Error(`${query}不是以${match}结束`)
+      }
+    } else {
+      if (query.startsWith(match.before, idx)) {
+        const next = match.next
+        let afterFlag = ''
+        if (typeof next == 'string') {
+          afterFlag = next
+        } else {
+          afterFlag = next.before
+        }
+        let value = ''
+        if (afterFlag) {
+          const nextIdx = query.indexOf(afterFlag, match.before.length + idx)
+          if (nextIdx < 0) {
+            throw new Error(`${query.slice(match.before.length + idx)}未找到${afterFlag}的分割`)
+          }
+          value = query.slice(idx + match.before.length, nextIdx)
+          idx = nextIdx + afterFlag.length - 1
+        } else {
+          value = query.slice(idx + match.before.length)
+        }
+        args = new KVPair(match.key, match.type(value), args)
+        match = match.next
+      } else {
+        throw new Error(`${query.slice(match.before.length + idx)}不是以${match.before}开始`)
+      }
+    }
+  }
+  return args
+}
+
+export function toMatchNode(node: string, typeDefMap: Record<string, (v: string) => any>) {
+  const abc = parseRule(new Que(node))
+  if (abc instanceof ParserSuccess) {
+    if (abc.end.notEnd()) {
+      throw new Error('未匹配到结束')
+    }
+    const nodes = abc.value
+    let ret: MatchNode = nodes.last
+    for (let i = nodes.before.length - 1; i > -1; i--) {
+      const node = nodes.before[i]
+      if (i != 0) {
+        if (!node.begin.length) {
+          throw new Error('两个[]之间不允许为空')
+        }
+      }
+      let type: any = quote
+      if (node.type) {
+        type = typeDefMap[node.type]
+        if (!type) {
+          throw new Error('未定义type:' + node.type)
+        }
+      }
+      ret = {
+        next: ret,
+        before: node.begin,
+        key: node.key,
+        type
+      }
+    }
+    return ret
+  }
+  throw abc
+}
+
+
+
+
