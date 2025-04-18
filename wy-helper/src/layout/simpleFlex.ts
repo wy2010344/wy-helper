13bf81a328d5793755d7e3fee4ea8736e8196a57
wy2010344 <wangyang2010344@foxmail.com>
2025-04-19 00:48:57 +0800

    more flex support

---
+++ wy-helper/src/layout/simpleFlex.ts
@@ -0,0 +1,47 @@
+
+
+/**
+ * 假设有N维
+ * 那个flex就会指向维度
+ * N维以map还是list?
+ * 自定义key还是数字?
+ */
+
+import { PointKey } from "../geometry"
+import { emptyObject } from "../util"
+import { CrossAxisConfig, DirectionFix, DirectionFixBetweenWhenOne, flexDisplayUtil, MainAxisConfig } from "./flexUtil"
+
+
+export function oppositeDirection(x: PointKey): PointKey {
+  if (x == 'x') {
+    return 'y'
+  } else {
+    return 'x'
+  }
+}
+
+export type DisplayProps = MainAxisConfig & {
+  direction?: PointKey
+} & CrossAxisConfig
+export function simpleFlex({
+  direction = 'y',
+  alignFix,
+  alignItems,
+  ...args
+}: DisplayProps) {
+  return flexDisplayUtil(direction, args, {
+    [direction as 'x']: emptyObject,
+    [oppositeDirection(direction) as 'y']: {
+      alignFix,
+      alignItems
+    }
+  })
+}
+
+/**
+ * stack布局,相当于没有主轴,都是辅助轴
+ * 在3D中,Z轴上flex,看上z轴,则是重叠
+ * 但3D有自己的stack.
+ * 2D也有stack
+ * 比较常用,是stretch下,多个块都重在一起.
+ */
--- wy-helper/src/layout/util.ts
+++ wy-helper/src/layout/util.ts
@@ -2,21 +2,21 @@ import { LayoutKey } from "."
 import { GetValue } from "../setStateHelper"
 import { ValueOrGet } from "../signal"
 
-export interface LayoutModel {
+export type LayoutModel<K extends string> = {
+  getSize(key: K): number
+  getPosition(key: K): number
   index(): number
-  x: GetValue<number>
-  y: GetValue<number>
-  width: GetValue<number>
-  height: GetValue<number>
-  getExt(): {
-    /**
-     * 这个应该在提供flex的地方过滤
-     * 便需要外部提供key
-     */
-    // notFlex?: boolean,
-    grow?: ValueOrGet<number>
-    align?: AlignSelfFun
-  }
+  getGrow(): number | void
+  getAlign(key: K): AlignSelfFun | void
+  // getExt(): {
+  //   /**
+  //    * 这个应该在提供flex的地方过滤
+  //    * 便需要外部提供key
+  //    */
+  //   // notFlex?: boolean,
+  //   grow?: ValueOrGet<number>
+  //   align?: Partial<Record<K, AlignSelfFun>>
+  // }
 }
 
 export type AlignSelfFun = {
@@ -25,17 +25,16 @@ export type AlignSelfFun = {
 }
 
 
-export interface HookInfo {
-  width: GetValue<number>
-  height: GetValue<number>
-  children: GetValue<LayoutModel[]>
+export interface HookInfo<K extends string> {
+  getSize(key: K): number
+  children: GetValue<LayoutModel<K>[]>
 }
 
 const m = globalThis as unknown as {
-  _wy_current_layout_children_: HookInfo
+  _wy_current_layout_children_: HookInfo<any>
 }
 export function hookLayout<T>(
-  info: HookInfo,
+  info: HookInfo<any>,
   calculate: GetValue<T>
 ) {
   const before = m._wy_current_layout_children_
