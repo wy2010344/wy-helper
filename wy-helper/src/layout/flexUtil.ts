109d47908165a8750fc898695b1f998cf66d776d
wy2010344 <wangyang2010344@foxmail.com>
2025-08-29 18:09:21 +0800

    code pretty

--- wy-helper/src/layout/flexUtil.ts
+++ wy-helper/src/layout/flexUtil.ts
@@ -1,11 +1,22 @@
-import { MDisplayOut, SizeKey } from "."
-import { arrayReduceLeft, arrayReduceRight } from "../equal"
-import { PointKey } from "../geometry"
-import { objectMap } from "../setStateHelper"
-import { getValueOrGet, memo, MemoFun, ValueOrGet, valueOrGetToGet } from "../signal"
-import { cacheGetFun, emptyObject } from "../util"
-import { AlignSelfFun, hookGetLayoutChildren, HookInfo, LayoutModel } from "./util"
-
+import { MDisplayOut, SizeKey } from '.'
+import { arrayReduceLeft, arrayReduceRight } from '../equal'
+import { PointKey } from '../geometry'
+import { GetValue, objectMap } from '../setStateHelper'
+import {
+  getValueOrGet,
+  memo,
+  MemoFun,
+  ValueOrGet,
+  valueOrGetToGet,
+} from '../signal'
+import { alawaysGet } from '../tokenParser'
+import { asLazy, cacheGetFun, emptyObject } from '../util'
+import {
+  AlignSelfFun,
+  hookGetLayoutChildren,
+  HookInfo,
+  LayoutModel,
+} from './util'
 
 export type MainAxisConfig = {
   reverse?: boolean
@@ -13,17 +24,22 @@ export type MainAxisConfig = {
   directionFix?: DirectionFix
   directionFixBetweenWhenOne?: DirectionFixBetweenWhenOne
   gap?: number
-
 }
 export type CrossAxisConfig = {
   alignItems?: AlignItem
-  /**辅助轴方向是否固定 */
+  /**辅助轴方向尺寸是否固定 */
   alignFix?: boolean
 }
 
 export type DirectionFixBetweenWhenOne = 'center' | 'end' | 'start'
 
-export type DirectionFix = 'start' | 'end' | 'center' | 'between' | 'around' | 'evenly'
+export type DirectionFix =
+  | 'start'
+  | 'end'
+  | 'center'
+  | 'between'
+  | 'around'
+  | 'evenly'
 
 export type AlignItem = 'center' | 'start' | 'end' | 'stretch'
 
@@ -35,7 +51,7 @@ export function alignSelf(getAlign: ValueOrGet<AlignItem>): AlignSelfFun {
       if (align == 'center') {
         return (pWidth - getSelfWidth()) / 2
       } else if (align == 'end') {
-        return (pWidth - getSelfWidth())
+        return pWidth - getSelfWidth()
       } else {
         return 0
       }
@@ -46,55 +62,54 @@ export function alignSelf(getAlign: ValueOrGet<AlignItem>): AlignSelfFun {
         return pWidth
       }
       throw 'you should make your own size'
-    }
+    },
   }
 }
 
-
 /**
  * 在ext里面使用align与grow,在控制在两个轴的伸长,还有notFlex
- * @param param0 
- * @returns 
+ * @param param0
+ * @returns
  */
 export function flexDisplayUtil<K extends string>(
   direction: K,
   axisConfig: MainAxisConfig = emptyObject,
-  align: AlignItemsConfig<K> = emptyObject,
+  align: AlignItemsConfig<K> = emptyObject
 ): MDisplayOut<K> {
   return new FlexDisplay(direction, axisConfig, align)
 }
 function directionToSize(x: PointKey): SizeKey {
   if (x == 'x') {
-    return "width"
+    return 'width'
   } else {
-    return "height"
+    return 'height'
   }
 }
 
-
-type AlignInfos<K extends string> = Record<K, {
-  alignItems: AlignItem,
-  alignFix: boolean
-  get: MemoFun<number>
-}>
+type AlignInfos<K extends string> = Record<
+  K,
+  {
+    alignItems: AlignItem
+    alignFix: boolean
+    get: GetValue<number>
+  }
+>
 
 type AlignItemsConfig<K extends string> = {
   [key in K]?: CrossAxisConfig
 } & CrossAxisConfig
+
+const lazy0 = asLazy(0)
 class StackDisplay<K extends string> implements MDisplayOut<K> {
   protected info: HookInfo<K>
   private alignInfos: AlignInfos<K> = {} as any
   constructor(align: AlignItemsConfig<K>) {
     this.info = hookGetLayoutChildren() as HookInfo<K>
-    const {
-      alignItems: dAlignItems = 'center',
-      alignFix: dAlignFix = false
-    } = align
-    this.info.forEach(key => {
-      const {
-        alignItems = dAlignItems,
-        alignFix = dAlignFix
-      } = align[key] || emptyObject as CrossAxisConfig
+    const { alignItems: dAlignItems = 'center', alignFix: dAlignFix = false } =
+      align
+    this.info.forEach((key) => {
+      const { alignItems = dAlignItems, alignFix = dAlignFix } =
+        align[key] || (emptyObject as CrossAxisConfig)
       this.createAlignInfo(key, alignItems, alignFix)
     })
   }
@@ -104,60 +119,74 @@ class StackDisplay<K extends string> implements MDisplayOut<K> {
       alignItems,
       alignFix,
       get: cacheGetFun(() => {
+        if (alignFix) {
+          return lazy0
+        }
         return memo(() => {
           let width = 0
           this.info.children().forEach((child) => {
-            if (!alignFix) {
-              const align = child.getAlign(key as K)
-              if (!align) {
-                width = Math.max(child.getSize(key as K), width)
-              }
+            const align = child.getAlign(key as K)
+            if (!align) {
+              width = Math.max(
+                child.getOuterSizeForParentLayout(key as K),
+                width
+              )
             }
           })
           return width
         })
-      })
+      }),
     }
   }
 
   getChildInfo(x: K, size: boolean, i: number): number {
     const child = this.info.children()[i]
-    const cf = this.alignInfos[x]
-    const theWidth = cf.alignFix ? this.info.getSize(x) : cf.get()
+    const { alignFix, get, alignItems } = this.alignInfos[x]
+    const theWidth = alignFix ? this.info.getInnerSizeToLayout(x) : get()
     const align = child.getAlign(x)
     if (align) {
       //对于辅轴,开始与宽度,如y与height,x与width
       if (size) {
         return align.size(theWidth)
       } else {
-        return align.position(theWidth, () => child.getSize(x))
+        return align.position(theWidth, () =>
+          child.getOuterSizeForParentLayout(x)
+        )
       }
-    } else if (cf.alignItems == 'stretch') {
+    }
+
+    if (alignItems == 'stretch') {
       if (size) {
         return theWidth
       } else {
         return 0
       }
-    } else {
-      if (cf.alignItems == 'start') {
-        return 0
-      } else if (cf.alignItems == 'center') {
-        return (theWidth - child.getSize(x)) / 2
-      } else if (cf.alignItems == 'end') {
-        return theWidth - child.getSize(x)
-      }
     }
-    throw ''
+
+    if (size) {
+      throw `child should have it's own size`
+    }
+
+    if (alignItems == 'start') {
+      return 0
+    }
+    if (alignItems == 'center') {
+      return (theWidth - child.getOuterSizeForParentLayout(x)) / 2
+    }
+    if (alignItems == 'end') {
+      return theWidth - child.getOuterSizeForParentLayout(x)
+    }
+    throw 'never reach'
   }
   getSizeInfo(x: K, def?: boolean): number {
     if (def) {
       return 0
     }
-    const cf = this.alignInfos[x]
-    if (cf.alignFix) {
-      throw 'self design fix'
+    const { alignFix, get } = this.alignInfos[x]
+    if (alignFix) {
+      throw `self sould have it's own size`
     }
-    return cf.get()
+    return get()
   }
 }
 export function stackDisplayUtil<K extends string>(
@@ -168,19 +197,22 @@ export function stackDisplayUtil<K extends string>(
   return new StackDisplay(align)
 }
 
-
 class FlexDisplay<K extends string> extends StackDisplay<K> {
   constructor(
     private direction: K,
     private axisConfig: MainAxisConfig,
     align: {
-      [P in K]?: CrossAxisConfig;
+      [P in K]?: CrossAxisConfig
     }
   ) {
     super(align as Record<K, CrossAxisConfig>)
   }
 
-  protected createAlignInfo(key: K, alignItems: AlignItem, alignFix: boolean): void {
+  protected createAlignInfo(
+    key: K,
+    alignItems: AlignItem,
+    alignFix: boolean
+  ): void {
     if (key == this.direction) {
       return
     }
@@ -212,19 +244,23 @@ class FlexDisplay<K extends string> extends StackDisplay<K> {
           growAll += grow
           growIndex.set(child.index(), grow)
         } else {
-          totalLength = totalLength + child.getSize(direction)
+          totalLength =
+            totalLength + child.getOuterSizeForParentLayout(direction)
         }
       })
 
       if (growAll) {
-        let remaing = this.info.getSize(direction) - (gap * children.length - gap) - totalLength
+        let remaing =
+          this.info.getInnerSizeToLayout(direction) -
+          (gap * children.length - gap) -
+          totalLength
         forEach(children, (child) => {
           const grow = growIndex.get(child.index())
           const childLength = grow
             ? remaing > 0
-              ? remaing * grow / growAll
+              ? (remaing * grow) / growAll
               : 0
-            : child.getSize(direction)
+            : child.getOuterSizeForParentLayout(direction)
           childLengths[child.index()] = childLength
           length = length + childLength + gap
           list.push(length)
@@ -232,7 +268,7 @@ class FlexDisplay<K extends string> extends StackDisplay<K> {
       } else if (directionFix) {
         let tGap = gap
 
-        let allRemaing = this.info.getSize(direction) - totalLength
+        let allRemaing = this.info.getInnerSizeToLayout(direction) - totalLength
         let remaing = allRemaing - (gap * children.length - gap)
 
         if (directionFix == 'center') {
@@ -260,14 +296,14 @@ class FlexDisplay<K extends string> extends StackDisplay<K> {
           tGap = rGap
         }
         forEach(children, (child) => {
-          const childLength = child.getSize(direction)
+          const childLength = child.getOuterSizeForParentLayout(direction)
           childLengths[child.index()] = childLength
           length = length + childLength + tGap
           list.push(length)
         })
       } else {
         forEach(children, (child) => {
-          const childLength = child.getSize(direction)
+          const childLength = child.getOuterSizeForParentLayout(direction)
           childLengths[child.index()] = childLength
           length = length + childLength + gap
           list.push(length)
@@ -284,7 +320,7 @@ class FlexDisplay<K extends string> extends StackDisplay<K> {
         childLengths,
         list,
         growAll,
-        length
+        length,
       }
     })
   })
@@ -304,7 +340,10 @@ class FlexDisplay<K extends string> extends StackDisplay<K> {
     }
     if (x == this.direction) {
       if (this.axisConfig.directionFix) {
-        throw 'parent or self should make size for directionFix:' + this.axisConfig.directionFix
+        throw (
+          'parent or self should make size for directionFix:' +
+          this.axisConfig.directionFix
+        )
       }
       const { growAll, length } = this.itGetInfo()
       if (growAll != 0) {
@@ -315,4 +354,3 @@ class FlexDisplay<K extends string> extends StackDisplay<K> {
     return super.getSizeInfo(x, def)
   }
 }
-
--- wy-helper/src/layout/layoutNode.ts
+++ wy-helper/src/layout/layoutNode.ts
@@ -15,39 +15,41 @@ import {
   objectMap,
   alignSelf,
   SetValue,
-} from "..";
+  HookInfo,
+} from '..'
 
 export function layoutNodeGetTarget<M, K extends string>(
   n: LayoutNode<M, K>
 ): M {
-  return n.target;
+  return n.target
 }
 
 /**
  * 不太对.矩形区域本质上相当于g、group只管布局.在布局之下,有默认的视图?
  * 即布局本身确实需要依附一个节点存在
  */
-export class LayoutNode<M, K extends string>
-  implements MDisplayOut<K>, LayoutModel<K>
-{
-  target!: M;
-  _index: number = 0;
-  _get: EmptyFun = emptyFun;
+export class LayoutNode<M, K extends string> implements LayoutModel<K> {
+  target!: M
+  _index: number = 0
+  _get: EmptyFun = emptyFun
   index() {
-    this._get();
-    return this._index;
+    this._get()
+    return this._index
   }
   constructor(
-    readonly getDisplay: GetValue<MDisplayOut<K>>,
+    private readonly getDisplay: GetValue<MDisplayOut<K>>,
     readonly axis: Record<
       K,
       {
-        position: GetValue<any>;
-        size: GetValue<any>;
-        paddingStart: GetValue<number>;
-        paddingEnd: GetValue<number>;
-        drawSize: GetValue<number>;
-        alignSelf?: AlignSelfFun;
+        /**外部尺寸,外部也会使用 */
+        size: GetValue<number>
+        /**外部坐标,主要是供外部使用 */
+        position: GetValue<number>
+        paddingStart: GetValue<number>
+        paddingEnd: GetValue<number>
+        /**内部尺寸,用于自身布局,也在文字绘制时使用 */
+        innerSize: GetValue<number>
+        alignSelf?: AlignSelfFun
       }
     >,
     readonly children: GetValue<readonly LayoutNode<M, K>[]>,
@@ -56,91 +58,51 @@ export class LayoutNode<M, K extends string>
   ) {}
 
   /**通过布局获得的信息 */
-  getSizeInfo(x: K, def?: boolean): number {
+  getOuterSizeFromLayout(x: K, def?: boolean): number {
     /**布局,从子节点汇总而来 */
-    const v = this.getDisplay().getSizeInfo(x, def);
-    const av = this.axis[x];
-    return v + av.paddingStart() + av.paddingEnd();
+    const v = this.getDisplay().getSizeInfo(x, def)
+    const av = this.axis[x]
+    return v + av.paddingStart() + av.paddingEnd()
   }
   getChildInfo(x: K, size: boolean, i: number): number {
-    const v = this.getDisplay().getChildInfo(x, size, i);
+    const v = this.getDisplay().getChildInfo(x, size, i)
     if (size) {
-      return v;
+      return v
     }
-    const av = this.axis[x];
-    return v + av.paddingStart();
+    const av = this.axis[x]
+    return v + av.paddingStart()
   }
 
-  /**为作为flex提供的节点 */
-  getSize(x: K): number {
-    const av = this.axis[x];
-    return av.size();
+  /**为作为flex提供的子节点 */
+  getOuterSizeForParentLayout(x: K): number {
+    const av = this.axis[x]
+    return av.size()
   }
   getAlign(key: K): AlignSelfFun | void {
-    return this.axis[key].alignSelf;
-  }
-  getPosition(x: K): number {
-    const av = this.axis[x];
-    return av.position();
+    return this.axis[key].alignSelf
   }
 }
 
 export interface LayoutNodeConfigure<M, K extends string> {
-  layout?: ((v: LayoutNode<M, K>) => MDisplayOut<K>) | MDisplayOut<K>;
+  layout?: ((v: LayoutNode<M, K>) => MDisplayOut<K>) | MDisplayOut<K>
   axis: Record<
     K,
     {
-      position?: InstanceCallbackOrValue<LayoutNode<M, K>>;
-      size?: InstanceCallbackOrValue<LayoutNode<M, K>>;
-      paddingStart?: ValueOrGet<number>;
-      paddingEnd?: ValueOrGet<number>;
-      alignSelf?: AlignSelfFun;
+      position?: InstanceCallbackOrValue<LayoutNode<M, K>>
+      size?: InstanceCallbackOrValue<LayoutNode<M, K>>
+      paddingStart?: ValueOrGet<number>
+      paddingEnd?: ValueOrGet<number>
+      alignSelf?: AlignSelfFun
     }
-  >;
-  grow?: ValueOrGet<number>;
-  notInLayout?: ValueOrGet<boolean>;
+  >
+  grow?: ValueOrGet<number>
+  notInLayout?: ValueOrGet<boolean>
 }
 
 export type LayoutConfig<M, K extends string> = {
-  getParentLayout(m: M): LayoutNode<M, K> | void;
-  getChildren(m: M): readonly M[];
-  getLayout(m: M): LayoutNode<M, K> | void;
-};
-/**
- * 没有显式定义的时候,如何取值.
- * @param x
- * @param size
- * @returns
- */
-function superCreateGet<M, K extends string>(size: boolean) {
-  return function (
-    getIns: GetValue<LayoutNode<M, K>>,
-    x: K,
-    c: LayoutConfig<M, K>
-  ) {
-    return function () {
-      const ins = getIns();
-      if (size) {
-        try {
-          //优先选择自己的,
-          const ix = ins.getSizeInfo(x);
-          return ix;
-        } catch (err) {
-          try {
-            return getFromParent(ins, c, x, size, err);
-          } catch (err) {
-            return ins.getSizeInfo(x, true);
-          }
-        }
-      } else {
-        try {
-          return getFromParent(ins, c, x, size, "define");
-        } catch (err) {
-          return 0;
-        }
-      }
-    };
-  };
+  getParentLayout(m: M): LayoutNode<M, K> | void
+  getChildren(m: M): readonly M[]
+  getLayout(m: M): LayoutNode<M, K> | void
 }
 
 function getFromParent<M, K extends string>(
@@ -148,114 +110,115 @@ function getFromParent<M, K extends string>(
   c: LayoutConfig<M, K>,
   x: K,
   size: boolean,
-  err: any
+  err: any = undefined,
+  def = 0
 ) {
-  if (ins.getNotInLayout()) {
-    throw err;
-  }
-  const parent = c.getParentLayout(ins.target);
+  const parent = c.getParentLayout(ins.target)
   if (parent && parent instanceof LayoutNode) {
-    return parent.getChildInfo(x, size, ins.index());
+    return parent.getChildInfo(x, size, ins.index())
   }
-  //其次选择来自父元素的约束
-  throw err;
-}
-
-const createGetPosition = superCreateGet(false);
-const createGetSize = superCreateGet(true);
-
-function emptyThrow(): number {
-  throw "abc";
-}
-
-function getInnerSize<M, K extends string>(
-  o: InstanceCallbackOrValue<LayoutNode<M, K>> | undefined,
-  c: LayoutConfig<M, K>,
-  getIns: GetValue<LayoutNode<M, K>>,
-  key: K,
-  begin: GetValue<number>,
-  end: GetValue<number>
-): GetValue<number> {
-  const tp = typeof o;
-  if (tp == "undefined") {
-    return function () {
-      return getFromParent(getIns(), c, key, true, "") - begin() - end();
-    };
-  } else if (tp == "number") {
-    return function () {
-      return (o as number) - begin() - end();
-    };
-  } else if (tp == "function") {
-    return function () {
-      return (o as any)(getIns()) - begin() - end();
-    };
-  } else {
-    return emptyThrow;
+  if (err) {
+    //其次选择来自父元素的约束
+    throw err
   }
+  return def
 }
+
 export function createLayoutNode<M, K extends string>(
   c: LayoutConfig<M, K>,
   n: LayoutNodeConfigure<M, K>
 ) {
   function getIns(): LayoutNode<M, K> {
-    return node;
+    return node
   }
-  const _layout = valueOrGetToGet(n.layout || absoluteDisplay);
+  const _layout = valueOrGetToGet(n.layout || absoluteDisplay)
 
   const axis = objectMap(n.axis, function (v, key) {
-    const size = valueInstOrGetToGet(v.size, getIns, createGetSize, key, c);
-    const paddingStart = valueOrGetToGet(v.paddingStart || 0);
-    const paddingEnd = valueOrGetToGet(v.paddingEnd || 0);
+    const paddingStart = valueOrGetToGet(v.paddingStart || 0)
+    const paddingEnd = valueOrGetToGet(v.paddingEnd || 0)
+    const defSize = valueInstOrGetToGet<number, LayoutNode<M, K>>(
+      v.size,
+      getIns
+    )
     return {
       alignSelf: v.alignSelf,
-      position: valueInstOrGetToGet(
-        v.position,
-        getIns,
-        createGetPosition,
-        key,
-        c
-      ),
-      size,
+      size:
+        defSize ||
+        function (): number {
+          try {
+            //优先从子节点的布局获取
+            const ix = node.getOuterSizeFromLayout(key)
+            return ix
+          } catch (err) {
+            try {
+              //从父节点的布局获取
+              return getFromParent(
+                node,
+                c,
+                key,
+                true,
+                err || `can't get size from layout`
+              )
+            } catch (err) {
+              //采用默认值
+              return node.getOuterSizeFromLayout(key, true)
+            }
+          }
+        },
+      position:
+        valueInstOrGetToGet<number, LayoutNode<M, K>>(v.position, getIns) ||
+        function (): number {
+          //从父节点的布局获取,或者默认0
+          return getFromParent(node, c, key, false)
+        },
       paddingStart,
       paddingEnd,
-      drawSize: getInnerSize(v.size, c, getIns, key, paddingStart, paddingEnd),
-    };
-  });
+      //内部尺寸,需要优先自身,再父布局,再子撑开
+      innerSize: defSize
+        ? function (): number {
+            return defSize() - paddingStart() - paddingEnd()
+          }
+        : function (): number {
+            const pw = getFromParent<M, K>(node, c, key, true, 'notfound')
+            return pw - paddingStart() - paddingEnd()
+          },
+    }
+  })
 
   const children = memo(() => {
     //生成复合结构,所以用memo
-    const list: LayoutNode<M, K>[] = [];
+    const list: LayoutNode<M, K>[] = []
     c.getChildren(node.target).forEach((child, i) => {
-      const rect = c.getLayout(child) as LayoutNode<M, K>;
+      const rect = c.getLayout(child) as LayoutNode<M, K>
       if (rect instanceof LayoutNode && !rect.getNotInLayout()) {
-        rect._index = list.length;
-        rect._get = children;
-        list.push(rect);
+        rect._index = list.length
+        rect._get = children
+        list.push(rect)
       }
-    });
-    return list;
-  });
-  const info = {
+    })
+    return list
+  })
+  const info: HookInfo<K> = {
     forEach(callback: SetValue<K>) {
       for (const key in axis) {
-        callback(key);
+        callback(key)
       }
     },
-    getSize(n: K) {
-      return axis[n].drawSize();
+    getInnerSizeToLayout(n: K) {
+      return axis[n].innerSize()
     },
     children,
-  };
+  }
   const layout: GetValue<MDisplayOut<K>> = memo(() => {
     //生成复全结构,所以用memo
-    return hookLayout(info, _layout);
-  });
+    return hookLayout(info, _layout)
+  })
   const node = new LayoutNode<M, K>(
     layout,
     axis,
-    info.children,
+    children,
     valueOrGetToGet(n.notInLayout || false),
     valueOrGetToGet(n.grow)
-  );
-  return node;
+  )
+  return node
 }
